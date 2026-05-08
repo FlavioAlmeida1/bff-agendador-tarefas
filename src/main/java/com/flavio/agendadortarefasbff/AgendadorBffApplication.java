cb9992e57080dc4e242a07c1ee62d41cc5b191e2
Flavio <flaviongo21@gmail.com>
2026-05-08 11:15:58 -0300

    adicionado esqueleto do projeto

---
+++ src/main/java/com/flavio/agendadortarefasbff/AgendadorBffApplication.java
@@ -0,0 +1,13 @@
+package com.flavio.agendadortarefasbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorBffApplication.class, args);
+	}
+
+}
